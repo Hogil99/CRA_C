470281b7c5ae1a3b93aba02014b9455c80f1eb6b
hui-ung.kim <hihiung@gmail.com>
2023-06-01 09:57:28 +0900

    add unit test for getDivide()

--- Calculator/Calculator/Sample-Test1/test.cpp
+++ Calculator/Calculator/Sample-Test1/test.cpp
@@ -4,3 +4,13 @@
 TEST(TestCaseName, TestName) {
 
 }
+
+TEST(TestCaseName, TestDivide) {
+	Cal cal;
+	EXPECT_EQ(100, cal.getDivide(1000, 10));
+}
+
+TEST(TestCaseName, TestDivideByZero) {
+	Cal cal;
+	EXPECT_EQ(0, cal.getDivide(1000, 0));
+}
