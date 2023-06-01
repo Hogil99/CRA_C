bc7c5a06ff86b48524bc51b7476bc0e85ad6ae23
apekel <jong3432@gmail.com>
2023-06-01 10:05:14 +0900

    add "getSum, getSumSum"

--- Calculator/Calculator/Calculator/Cal.cpp
+++ Calculator/Calculator/Calculator/Cal.cpp
@@ -2,5 +2,14 @@
 
 class Cal
 {
-	
+public:
+	int getSum(int a, int b)
+	{
+		return a + b;
+	}
+
+	int getSumSum(int a, int b, int c)
+	{
+		return a + b + c;
+	}
 };
--- Calculator/Calculator/Sample-Test1/test.cpp
+++ Calculator/Calculator/Sample-Test1/test.cpp
@@ -2,5 +2,14 @@
 #include "../Calculator/Cal.cpp"
 
 TEST(TestCaseName, TestName) {
+	Cal* cal = new Cal();
+	EXPECT_TRUE(cal != nullptr);
+}
 
+TEST(SumTest, TestName) {
+	EXPECT_EQ(Cal().getSum(1, 5), 6);
+}
+
+TEST(SumSumTest, TestName) {
+	EXPECT_EQ(Cal().getSumSum(1, 5, 10), 16);
 }
